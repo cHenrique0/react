49a0110e9b295603b53f73b08dfecc0c541596b7
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-06-26 16:20:51 -0300

    Learning about props

--- fundamentals/src/App.jsx
+++ fundamentals/src/App.jsx
@@ -1,7 +1,24 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  const posts = [
+    {
+      author: "Alice",
+      comment: "Hello, World!",
+    },
+    {
+      author: "Bob",
+      comment: "Hi, there!",
+    },
+  ];
+
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Post key={index} author={post.author} comment={post.comment} />
+      ))}
+    </main>
+  );
 }
 
 export default App;
--- fundamentals/src/components/Post.jsx
+++ fundamentals/src/components/Post.jsx
@@ -1,21 +1,8 @@
-const posts = [
-  {
-    username: "Alice",
-    content: "Hello, World!",
-  },
-  {
-    username: "Bob",
-    content: "Hi, there!",
-  },
-];
-
-export default function Post() {
-  const chosenPost = Math.random() > 0.5 ? posts[0] : posts[1];
-
+export default function Post(props) {
   return (
     <div>
-      <p>{chosenPost.username}</p>
-      <p>{chosenPost.content}</p>
+      <p>{props.author}</p>
+      <p>{props.comment}</p>
     </div>
   );
 }
