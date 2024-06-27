3ffd6167498594e61167ebe0cfea0b868c65caa9
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-06-27 10:38:08 -0300

    more about states

--- fundamentals/src/components/Post.jsx
+++ fundamentals/src/components/Post.jsx
@@ -1,10 +1,10 @@
 import styles from "./Post.module.css";
 
-export default function Post(props) {
+export default function Post({ author, comment }) {
   return (
     <div className={styles.post}>
-      <p className={styles.author}>{props.author}</p>
-      <p className={styles.text}>{props.comment}</p>
+      <p className={styles.author}>{author}</p>
+      <p className={styles.text}>{comment}</p>
     </div>
   );
 }
--- fundamentals/src/components/PostsList.jsx
+++ fundamentals/src/components/PostsList.jsx
@@ -1,11 +1,17 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import styles from "./PostsList.module.css";
 import { useState } from "react";
 
 export default function PostsList() {
   const [text, setText] = useState("");
   const [name, setName] = useState("");
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function inputTextHandler(event) {
     setText(event.target.value);
@@ -17,10 +23,14 @@ export default function PostsList() {
 
   return (
     <>
-      <NewPost
-        onChangeText={inputTextHandler}
-        onChangeName={inputNameHandler}
-      />
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onChangeText={inputTextHandler}
+            onChangeName={inputNameHandler}
+          />
+        </Modal>
+      )}
       <ul className={styles.posts}>
         <Post author={name} comment={text} />
         <Post author="Alice" comment="Hello World!" />
