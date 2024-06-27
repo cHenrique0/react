2e7e748b6cfc9cf5f9197ea202282311ffbb8bc3
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-06-27 11:17:22 -0300

    adding submit button

--- fundamentals/src/components/NewPost.jsx
+++ fundamentals/src/components/NewPost.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import styles from "./NewPost.module.css";
 
-export default function NewPost({ onChangeText, onChangeName, onCancel }) {
+export default function NewPost({ onCancel }) {
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+
+  function inputTextHandler(event) {
+    setText(event.target.value);
+  }
+
+  function inputNameHandler(event) {
+    setName(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const id = Math.floor(Math.random() * 10000);
+    const postData = { id, name, text };
+    console.log(postData);
+    onCancel();
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onChangeText} />
+        <textarea id="body" required rows={3} onChange={inputTextHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onChangeName} />
+        <input type="text" id="name" required onChange={inputNameHandler} />
       </p>
       <p className={styles.actions}>
         <button type="button" onClick={onCancel}>
--- fundamentals/src/components/PostsList.jsx
+++ fundamentals/src/components/PostsList.jsx
@@ -2,33 +2,16 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import styles from "./PostsList.module.css";
-import { useState } from "react";
 
 export default function PostsList({ isPosting, onStopPosting }) {
-  const [text, setText] = useState("");
-  const [name, setName] = useState("");
-
-  function inputTextHandler(event) {
-    setText(event.target.value);
-  }
-
-  function inputNameHandler(event) {
-    setName(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onChangeText={inputTextHandler}
-            onChangeName={inputNameHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={styles.posts}>
-        <Post author={name} comment={text} />
         <Post author="Alice" comment="Hello World!" />
       </ul>
     </>
