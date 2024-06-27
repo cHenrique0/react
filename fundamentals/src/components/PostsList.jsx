4d36b9e0fd416fe06734340da61a243caa87b3f9
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-06-27 17:03:15 -0300

    adding loading state

--- fundamentals/src/components/PostsList.jsx
+++ fundamentals/src/components/PostsList.jsx
@@ -6,12 +6,15 @@ import { useEffect, useState } from "react";
 
 export default function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8000/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -28,16 +31,22 @@ export default function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddNewPost={addNewPost} />
         </Modal>
       )}
-      {posts.length > 0 ? (
+      {!isFetching && posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post) => (
             <Post key={post.id} author={post.name} comment={post.text} />
           ))}
         </ul>
-      ) : (
+      )}
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "#FFF" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "#FFF" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
