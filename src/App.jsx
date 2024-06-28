0bc776dedf3779fa4aeef7c0fda28dda472bdf39
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-06-28 17:42:47 -0300

    Adding button component

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Profile from "./components/SLP/Profile"; // Desafio SLP
+
+function App() {
+  return <Profile />;
+}
+
+export default App;
---
+++ src/components/SLP/Button.jsx
@@ -0,0 +1,5 @@
+import styles from "./Button.module.css";
+
+export default function Button({ name }) {
+  return <button className={styles.button}>{name}</button>;
+}
