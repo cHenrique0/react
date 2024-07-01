d4b2e83616af9fb2883326f43a66812b53eae4a1
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-07-01 10:03:42 -0300

    Refactor challengeCard component and styles

--- src/Main/components/challengeCard.jsx
+++ src/Main/components/challengeCard.jsx
@@ -3,23 +3,22 @@ import styles from "../styles/challengeCard.module.css";
 export default function ChallengeCard({
   level,
   title,
-  description,
   image,
   link,
   borderColor,
 }) {
   return (
-    <div
-      className={styles.borderTop}
-      style={{ backgroundColor: `var(${borderColor})` }}
-    >
-      <div className={styles.card}>
+    <div className={styles.card}>
+      <div
+        className={styles.borderTop}
+        style={{ backgroundColor: `var(${borderColor})` }}
+      ></div>
+      <div className={styles.content}>
         <span>{level}</span>
         <h2>{title}</h2>
         <a href={link} target="_blank">
           <img src={image} />
         </a>
-        <p>{description}</p>
       </div>
     </div>
   );
