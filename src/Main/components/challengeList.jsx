d61c071f7d70d36d575aa058ab96e055f74efcf7
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-07-01 17:44:17 -0300

    refactor: Update challengeList component to use random border colors

--- src/Main/components/challengeList.jsx
+++ src/Main/components/challengeList.jsx
@@ -2,6 +2,19 @@ import ChallengeCard from "./challengeCard";
 import styles from "../styles/challengeList.module.css";
 
 export default function ChallengeList() {
+  function randomColor() {
+    const colors = [
+      "--index-light-red",
+      "--index-soft-blue",
+      "--index-light-red-v2",
+      "--index-lime-green",
+      "--index-violet",
+      "--index-soft-orange",
+    ];
+
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
   return (
     <div className={styles.list}>
       <ChallengeCard
@@ -9,14 +22,14 @@ export default function ChallengeList() {
         title="Social Links Profile"
         link="https://www.frontendmentor.io/challenges/social-links-profile-UG32l9m6dQ"
         image="/SLP/design.jpg"
-        borderColor="--index-lime-green"
+        borderColor={randomColor()}
       />
       <ChallengeCard
         level="Easy"
         title="Cards Feature Section"
         link="https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK"
         image="/FCFS/design.jpg"
-        borderColor="--index-lime-green"
+        borderColor={randomColor()}
       />
     </div>
   );
